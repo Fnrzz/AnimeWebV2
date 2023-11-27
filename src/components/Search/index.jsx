284bd0b52f57bb68ba1eee8992e2d16d7ab139d9
Fnrzz <faridnurrganteng123@gmail.com>
2023-11-27 23:18:38 +0700

    update Bug input Search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -15,19 +15,21 @@ const Search = ({ keyword }) => {
   return (
     <div className="relative">
       <form onSubmit={heandleSearch}>
-        <input
-          type="text"
-          className="rounded-l-[8px]  md:px-[16px] md:py-[8px] px-2 py-1 w-full focus:outline-none"
-          ref={searchRef}
-          defaultValue={keyword}
-        />
-        <button
-          type="submit"
-          className="bg-white absolute md:px-[16px] md:py-[8px] px-2 py-1 rounded-r-[8px] text-mycolor-primary font-bold"
-          onClick={heandleSearch}
-        >
-          Search
-        </button>
+        <div className="block w-full  ">
+          <input
+            type="text"
+            className="rounded-l-[8px] w-[80%]  md:px-[16px] md:py-[8px] px-2 py-1  focus:outline-none"
+            ref={searchRef}
+            defaultValue={keyword}
+          />
+          <button
+            type="submit"
+            className="bg-white absolute md:px-[16px] md:py-[8px] px-2 py-1 rounded-r-[8px] text-mycolor-primary font-bold"
+            onClick={heandleSearch}
+          >
+            Search
+          </button>
+        </div>
       </form>
     </div>
   );
