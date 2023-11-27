b8dcc40c4d71f908f67a15be6fa0790747f69b26
Fnrzz <faridnurrganteng123@gmail.com>
2023-11-27 20:56:44 +0700

    update Bug input Search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -16,13 +16,13 @@ const Search = ({ keyword }) => {
     <form onSubmit={heandleSearch}>
       <input
         type="text"
-        className="rounded-l-[8px]  px-[16px] py-[8px]  focus:outline-none"
+        className="rounded-l-[8px]  md:px-[16px] md:py-[8px] px-2 py-1  focus:outline-none"
         ref={searchRef}
         defaultValue={keyword}
       />
       <button
         type="submit"
-        className="bg-white px-[16px] py-[8px] rounded-r-[8px] text-mycolor-primary font-bold"
+        className="bg-white md:px-[16px] md:py-[8px] px-2 py-1 rounded-r-[8px] text-mycolor-primary font-bold"
         onClick={heandleSearch}
       >
         Search
