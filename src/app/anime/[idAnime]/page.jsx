1944cf265830fbbe74e9fa6a28e3dc23402c5d5f
Fnrzz <faridnurrganteng123@gmail.com>
2023-11-30 20:01:28 +0700

    [PUSH] fix detail anime

--- src/app/anime/[idAnime]/page.jsx
+++ src/app/anime/[idAnime]/page.jsx
@@ -14,7 +14,7 @@ const Page = async ({ params: { idAnime } }) => {
       <div className="my-[28px] w-full">
         <VideoPlayer idYoutube={dataAnime.trailer.youtube_id} />
         <div className="flex mt-[16px]">
-          <div className="block w-[130px] h-[250px] md:w-[250px] md:h-[450px] ">
+          <div className="block w-[130px]   md:w-[250px] ">
             <Image
               src={dataAnime.images.jpg.image_url}
               alt={dataAnime.title}
@@ -26,50 +26,52 @@ const Page = async ({ params: { idAnime } }) => {
             />
           </div>
           <div className="flex  flex-col ml-[16px] w-[calc(100%-150px)]">
-            <h1 className="text-[10px] md:text-2xl font-bold text-white">
+            <h1 className="text-[14px] md:text-2xl font-bold text-white">
               {dataAnime.title}
             </h1>
-            <h2 className="text-[8px] md:text-sm font-bold text-white">
+            <h2 className="text-[12px] md:text-sm font-bold text-white">
               {dataAnime.title_japanese}
             </h2>
             <div className="flex gap-[8px] mt-[8px]">
-              <p className="text-[8px] md:text-sm font-bold text-white ">
+              <p className="text-[10px] md:text-sm font-bold text-white ">
                 Rating : {dataAnime.score} / 10
               </p>
-              <p className="text-[8px] md:text-sm font-bold text-white">
+              <p className="text-[10px] md:text-sm font-bold text-white">
                 Rank : {dataAnime.rank}
               </p>
             </div>
             <div className="flex gap-[8px] mt-[4px]">
-              <p className="text-[8px] md:text-sm font-bold text-white ">
+              <p className="text-[10px] md:text-sm font-bold text-white ">
                 Type : {dataAnime.type}
               </p>
-              <p className="text-[8px] md:text-sm font-bold text-white">
+              <p className="text-[10px] md:text-sm font-bold text-white">
                 Duration : {dataAnime.duration}
               </p>
             </div>
             <div className="flex gap-[8px] mt-[4px]">
-              <p className="text-[8px] md:text-sm font-bold text-white ">
+              <p className="text-[10px] md:text-sm font-bold text-white ">
                 Season : {dataAnime.season}
               </p>
-              <p className="text-[8px] md:text-sm font-bold text-white">
+              <p className="text-[10px] md:text-sm font-bold text-white">
                 Year : {dataAnime.year}
               </p>
             </div>
             <div className="flex flex-col mt-[4px]">
-              <p className="text-[8px] md:text-sm font-bold text-white ">
+              <p className="text-[10px] md:text-sm font-bold text-white ">
                 Genre : {dataAnime.genres.map((genre) => genre.name).join(", ")}
               </p>
             </div>
             <p className="hidden md:block text-sm font-bold text-white mt-[4px] ">
               Synopsis :
             </p>
-            <p className="  text-[4px] md:text-[12px] font-bold text-white mt-[4px] ">
+            <p className="hidden md:block md:text-[12px] font-bold text-white mt-[4px] ">
               {dataAnime.synopsis}
             </p>
           </div>
         </div>
-        
+        <div className="flex md:hidden mt-[8px]">
+          <p className="text-[8px] font-bold text-white">{dataAnime.synopsis}</p>
+        </div>
       </div>
     </main>
   );
